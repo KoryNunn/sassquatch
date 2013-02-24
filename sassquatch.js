518d4e8714b2ff4c2ef995bffed570c713f814f9
unknown <knunn187@gmail.com>
2013-02-25 00:42:05 +1100

    cleaned up a bit, handled errors better

--- sassquatch.js
+++ sassquatch.js
@@ -1,26 +1,44 @@
 var scss = require('node-sass'),
 	fs = require('fs'),
 	path = '../';
+	
+function hasError(error){
+	if(error){
+		console.log(error);
+		return true;
+	}
+}
+	
+function renderSass(callback){
+	return function(filename, sass){
+		scss.render(sass.toString(), function(error, css){
+			hasError(error) || callback(filename, css);
+		});
+	};
+}
+	
+function readSass(filename, callback){
+	fs.readFile(path + filename, function(error, scssFile){
+		hasError(error) || callback(filename, scssFile.toString());
+	});
+}
+
+function writeCss(filename, css){	
+	fs.writeFile(path + filename.split('.').slice(0,-1).join('.') + '.css', css, function(error){
+		hasError(error);
+	});
+}
 
 fs.watch(path, function(eventType, filename){
-	var ext = filename.split('.').pop();
+	var ext;
+
+	if(eventType !== 'change'){
+		return;
+	}
+
+	ext = filename.split('.').pop();
+
 	if(ext === 'scss' || ext === 'sass'){
-		fs.readFile(path + filename, function(error, scssFile){
-			if(error){
-				console.log(error);
-				return;
-			}
-			scss.render(scssFile.toString(), function(error, css){
-				if(error){
-					console.log(error);
-					return;
-				}
-				fs.writeFile(path + filename.split('.').slice(0,-1).join('.') + '.css', css, function(error){
-					if(error){
-						console.log(error);
-					}
-				});
-			});			
-		})
+		readSass(filename, renderSass(writeCss));
 	}
 })
